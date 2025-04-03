98b0a174485d1dc2dd9ff81c432875835f32f71a
pedro <contatopedrogurgel@gmail.com>
2025-04-03 17:22:53 -0300

    instaling pest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,19 +1,8 @@
 <?php
 
-namespace Tests\Feature;
+use function PHPUnit\Framework\assertTrue;
 
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+it('should works', function () {
 
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
-
-        $response->assertStatus(200);
-    }
-}
+    assertTrue(true);
+});
