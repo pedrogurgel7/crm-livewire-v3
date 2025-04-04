4ba7c799e988ab09a235ec300d73089bc0af5884
pedro <contatopedrogurgel@gmail.com>
2025-04-04 20:00:03 -0300

    LIVE-3: register frontend

---
+++ resources/views/components/layouts/guest.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="min-h-screen font-sans antialiased bg-base-200">
+
+{{-- NAVBAR mobile only --}}
+<x-nav sticky class="lg:hidden">
+    <x-slot:brand>
+        <x-app-brand />
+    </x-slot:brand>
+    <x-slot:actions>
+        <label for="main-drawer" class="lg:hidden me-3">
+            <x-icon name="o-bars-3" class="cursor-pointer" />
+        </label>
+    </x-slot:actions>
+</x-nav>
+
+{{-- MAIN --}}
+<x-main>
+    {{-- The `$slot` goes here --}}
+    <x-slot:content>
+        {{ $slot }}
+    </x-slot:content>
+</x-main>
+
+{{--  TOAST area --}}
+<x-toast />
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,9 @@
 
 use Livewire\Volt\Volt;
 
-Volt::route('/', 'users.index');
+//Volt::route('/', 'users.index');
+
+Route::get('/', \App\Livewire\Welcome::class);
+
+Route::get('register', \App\Livewire\Auth\Register::class)->name('auth.register');
+Route::get('/logout', fn () => auth()->logout());
