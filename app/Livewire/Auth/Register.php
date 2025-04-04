b3f04cf2c4243589a50bc849d1e47a26867d72f5
pedro <contatopedrogurgel@gmail.com>
2025-04-04 18:31:36 -0300

    LIVE-3: should be able to register a user

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -2,12 +2,31 @@
 
 namespace App\Livewire\Auth;
 
+use App\Models\User;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Register extends Component
 {
-    public function render()
+    public string $name;
+
+    public string $email;
+
+    public string $email_confirmation;
+
+    public string $password;
+
+    public function render(): View
     {
         return view('livewire.auth.register');
+    }
+
+    public function submit(): void
+    {
+        User::query()->create([
+            'name'     => $this->name,
+            'email'    => $this->email,
+            'password' => $this->password,
+        ]);
     }
 }
--- tests/Feature/Livewire/Auth/RegisterTest.php
+++ tests/Feature/Livewire/Auth/RegisterTest.php
@@ -8,3 +8,19 @@ it('renders successfully', function () {
         ->assertStatus(200);
 
 });
+
+it('should be able to register a user', function () {
+    Livewire::test(Register::class)
+        ->set('name', 'John dee')
+        ->set('email', 'contato@gmail.com')
+        ->set('email_confirmation', 'contato@gmail.com')
+        ->set('password', 'password')->call('submit')
+
+        ->assertHasNoErrors();
+
+    \Pest\Laravel\assertDatabaseHas('users', [
+        'name' => 'John dee',
+    ]);
+
+    \Pest\Laravel\assertDatabaseCount('users', 1);
+});
