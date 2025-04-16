f362d7b3ac53d95026bd01562b1e466ec55e9399
pedro <contatopedrogurgel@gmail.com>
2025-04-16 14:26:38 -0300

    LIVE-7: should be able to give an user a permission to do something

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,15 +5,16 @@ namespace App\Models;
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Auth\Passwords\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
+    use CanResetPassword;
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory;
     use Notifiable;
-    use CanResetPassword;
 
     /**
      * The attributes that are mass assignable.
@@ -35,6 +36,22 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    public function givePermissionTo(string $key)
+    {
+        // $permission = Permission::query()->firstOrCreate(['key' => $key]);
+        $this->permissions()->firstOrCreate(compact('key'));
+    }
+
+    public function hasPermissionTo(string $key): bool
+    {
+        return $this->permissions()->where('key', $key)->exists();
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class);
+    }
 
     /**
      * Get the attributes that should be cast.
---
+++ tests/Feature/PermissionControlTest.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\{Permission, User};
+
+use function Pest\Laravel\assertDatabaseHas;
+
+it('should be able to give an user a permission to do something', function () {
+    /** @var User $user */
+    $user = User::factory()->create();
+    $user->givePermissionTo('be an admin');
+
+    expect($user)->hasPermissionTo('be an admin')->toBeTrue();
+
+    \Pest\Laravel\assertDatabaseHas('permissions', [
+        'key' => 'be an admin',
+    ]);
+
+    assertDatabaseHas('permission_user', [
+        'user_id'       => $user->id,
+        'permission_id' => Permission::where('key', 'be an admin')->first()->id,
+    ]);
+});
